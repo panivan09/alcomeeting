7b62a34b87dda4636a4ada419d66e31c1dc573b6
Ivan Panteleimonov <panteleimonov09@gmail.com>
2022-10-25 12:18:16 +0300

    Add .gitignore

---
+++ src/main/java/com/ivan/alcomeeting/AlcomeetingApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.alcomeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcomeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcomeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivan/alcomeeting/AlcomeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.alcomeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcomeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
