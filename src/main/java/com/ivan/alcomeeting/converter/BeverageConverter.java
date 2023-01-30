63d562ff4aa4c7a19b1e0a301353813b757553ea
panivan09 <panteleimonov09@gmail.com>
2023-01-30 16:31:38 +0200

    Add Test for BeverageConverter

--- src/main/java/com/ivan/alcomeeting/converter/BeverageConverter.java
+++ src/main/java/com/ivan/alcomeeting/converter/BeverageConverter.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Component;
 public class BeverageConverter {
 
     public BeverageDto beverageToBeverageDto(Beverage beverage){
-
         BeverageDto beverageDto = new BeverageDto();
-
         beverageDto.setId(beverage.getId());
         beverageDto.setName(beverage.getName());
         beverageDto.setDescription(beverage.getDescription());
@@ -19,14 +17,11 @@ public class BeverageConverter {
     }
 
     public Beverage beverageDtoToBeverage(BeverageDto beverageDto){
-
         Beverage beverage = new Beverage();
-
         beverage.setId(beverageDto.getId());
         beverage.setName(beverageDto.getName());
         beverage.setDescription(beverageDto.getDescription());
 
         return beverage;
     }
-
 }
---
+++ src/test/java/com/ivan/alcomeeting/converter/BeverageConverterTest.java
@@ -0,0 +1,47 @@
+package com.ivan.alcomeeting.converter;
+
+import com.ivan.alcomeeting.dto.BeverageDto;
+import com.ivan.alcomeeting.entity.Beverage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeverageConverterTest {
+    @InjectMocks
+    private BeverageConverter beverageConverter;
+
+    @BeforeEach
+    public void beforeTests(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void beverageToBeverageDto_returnBeverageDtoAfterConverter() {
+        // Given
+        Beverage beverage = new Beverage(1L, "Shmurdyak", "Unknown");
+        BeverageDto expected = new BeverageDto(1L, "Shmurdyak", "Unknown");
+
+        // When
+        BeverageDto actual = beverageConverter.beverageToBeverageDto(beverage);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void beverageDtoToBeverage_returnBeverageAfterConverter() {
+        // Given
+        BeverageDto beverageDto = new BeverageDto(1L, "Shmurdyak", "Unknown");
+        Beverage expected = new Beverage(1L, "Shmurdyak", "Unknown");
+
+        // When
+        Beverage actual = beverageConverter.beverageDtoToBeverage(beverageDto);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
